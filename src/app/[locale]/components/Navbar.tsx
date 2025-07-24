cf904498cfabfe959ce4228c49f4dca77cd4362f
quantum-builds <muhammadmassab.dev@gmail.com>
2025-07-24 15:32:53 +0500

    fix: navigation

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -1,22 +1,52 @@
 "use client";
 import { VerdantGreenLogo } from "@/assets";
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname, useRouter } from "@/i18n/navigation";
 import { Menu, X } from "lucide-react";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
 import { useState } from "react";
 
+interface NavOption {
+  label: string;
+  href?: string;
+  hash?: string;
+}
+
 export default function Navbar() {
   const t = useTranslations("Navigation");
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const pathname = usePathname();
+  const router = useRouter();
 
-  const navItems = [
-    { name: t("projects"), href: "#projects" },
-    { name: t("services"), href: "#services" },
-    { name: t("features"), href: "#features" },
-    { name: t("workflow"), href: "#workflow" },
-    { name: t("blog"), href: "#blog" },
+  const navItems: NavOption[] = [
+    { label: t("projects"), hash: "projects" },
+    // { name: t("services"), hash: "services" },
+    { label: t("features"), hash: "features" },
+    { label: t("workflow"), hash: "workflow" },
+    { label: t("blog"), hash: "blog" },
   ];
+
+  const handleNavigation = (option: NavOption) => {
+    if (option.href) {
+      router.push(option.href);
+      setIsMobileMenuOpen(false);
+      return;
+    }
+
+    const targetPath = "/";
+    const targetHash = `#${option.hash}`;
+
+    if (pathname === targetPath && option.hash) {
+      const el = document.getElementById(option.hash);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    } else if (option.hash) {
+      router.push(`${targetPath}${targetHash}`);
+    }
+
+    setIsMobileMenuOpen(false);
+  };
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-gray backdrop-blur-sm font-inter">
       <div className="max-w-11/12  mx-auto ">
@@ -26,13 +56,13 @@ export default function Navbar() {
           </Link>
           <div className="hidden lg:flex items-center lg:space-x-6 xl:space-x-10">
             {navItems.map((item) => (
-              <Link
-                key={item.name}
-                href={item.href}
+              <button
+                key={item.label}
+                onClick={() => handleNavigation(item)}
                 className=" hover:text-[#56aeff] transition-colors duration-200 text-lg scroll-smooth block w-fit"
               >
-                {item.name}
-              </Link>
+                {item.label}
+              </button>
             ))}
           </div>
           <Link
@@ -58,14 +88,13 @@ export default function Navbar() {
           <div className="lg:hidden absolute top-full left-0 right-0 bg-gray  backdrop-blur-lg border-y-1 border-gray-200  ">
             <div className="px-6 py-4 space-y-4">
               {navItems.map((item, index) => (
-                <Link
+                <button
                   key={index}
-                  href={item.href}
                   className="block text-black hover:text-[#56aeff] transition-colors duration-200 text-lg font-medium py-2"
-                  onClick={() => setIsMobileMenuOpen(false)}
+                  onClick={() => handleNavigation(item)}
                 >
-                  {item.name}
-                </Link>
+                  {item.label}
+                </button>
               ))}
               <a
                 className="w-full  bg-black hover:bg-green-gradient text-white cursor-pointer px-6 py-3 rounded-lg flex items-center justify-center gap-2 transition-all duration-200 font-medium mt-4  text-lg font-inter"

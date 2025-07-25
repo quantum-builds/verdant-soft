d1a4d8117c247a355010041b5bfad5e35cd11a9d
quantum-builds <muhammadmassab.dev@gmail.com>
2025-07-25 22:39:43 +0500

    add: all-blog page

--- src/app/[locale]/all-blogs/components/sections/BlogSection.tsx
+++ src/app/[locale]/all-blogs/components/sections/BlogSection.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { Arrow, Blog1, Blog2, Blog3 } from "@/assets";
-import { useRouter } from "@/i18n/navigation";
-import { slideFromBottom, slideFromTop } from "@/uitls/sliderAnimation";
+import Navbar from "@/app/[locale]/components/Navbar";
+import { Blog1, Blog2, Blog3 } from "@/assets";
+import { slideFromBottom } from "@/uitls/sliderAnimation";
 import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 import { StaticImageData } from "next/image";
@@ -14,7 +14,6 @@ interface IBlog {
 }
 export default function BlogSection() {
   const t = useTranslations("BlogSection");
-  const router = useRouter();
 
   const BLOGS: IBlog[] = [
     {
@@ -33,35 +32,29 @@ export default function BlogSection() {
       link: "/",
     },
   ];
+
+  const slideFromTop = {
+    hidden: { opacity: 0, y: -20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+    },
+  };
   return (
-    <section id="blogs" className="scroll-mt-28">
-      <div className="min-h-[70vh] 3xl:min-h-[50vh] w-11/12 xl:w-10/12  mx-auto mb-28 flex flex-col gap-12 overflow-hidden">
-        <p className="font-semibold text-2xl">[06 Blog]</p>
-        <div className="w-full flex justify-end">
-          <motion.h2
-            className="w-11/12 md:w-2/3 lg:w-3/5 2xl:w-8/12 text-2xl md:text-3xl lg:text-4xl xl:text-5xl 3xl:text-[54px] 4xl:text-6xl 5xl:text-7xl  font-semibold text-end break-words"
-            initial="hidden"
-            whileInView="visible"
-            variants={slideFromTop}
-            transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
-            viewport={{ once: true }}
-          >
-            {t.rich("headline", {
-              highlight: (chunks) => (
-                <span className="text-green-gradient">{chunks}</span>
-              ),
-            })}
-          </motion.h2>
-        </div>
-        <div
-          className="flex gap-2 cursor-pointer items-center w-fit"
-          onClick={() => router.push("/all-blogs")}
+    <section className="relative flex flex-col space-y-6 overflow-hidden bg-white mb-28 ">
+      <Navbar />
+      <div className="min-h-[30vh] mt-24 md:mt-30 xl:mt-36  w-11/12 xl:w-10/12  mx-auto flex flex-col gap-12 xl:gap-16 overflow-hidden">
+        <motion.h2
+          className="text-3xl lg:text-4xl xl:text-5xl 3xl:text-[54px] 4xl:text-6xl 5xl:text-7xl leading-tight font-semibold text-start break-words text-green-gradient"
+          initial="hidden"
+          whileInView="visible"
+          variants={slideFromTop}
+          transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
+          viewport={{ once: true }}
         >
-          <p className="text-[#707070] text-xl  md:text-2xl xl:text-4xl font-semibold">
-            All Blogs
-          </p>
-          <Image src={Arrow} alt="arrow" width={30} />
-        </div>
+          All Blogs
+        </motion.h2>
+
         <motion.div
           className="flex-1 lg:flex
     lg:flex-row
---
+++ src/app/[locale]/all-blogs/page.tsx
@@ -0,0 +1,11 @@
+import FooterSection from "../components/sections/FooterSection";
+import BlogSection from "./components/sections/BlogSection";
+
+export default function AllBlogs() {
+  return (
+    <>
+      <BlogSection />
+      <FooterSection />
+    </>
+  );
+}
